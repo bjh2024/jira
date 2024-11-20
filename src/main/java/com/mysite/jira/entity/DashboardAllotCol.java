dadb594d38ae9fb31f15bc69b411e698c81de800
ITSC <ITSC@ISTC>
2024-11-20 12:41:03 +0900

    hihi

---
+++ src/main/java/com/mysite/jira/entity/DashboardAllotCol.java
@@ -0,0 +1,26 @@
+package com.mysite.jira.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class DashboardAllotCol {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "allot_col_seq")
+	@SequenceGenerator(name = "allot_col_seq", sequenceName = "allot_col_seq", allocationSize = 1)
+	private Integer idx;
+	
+	@ManyToOne
+	private DashboardAllot dashboardAllot;
+	
+	@ManyToOne
+	private  DashboardCol dashboardCol; 
+}
---
+++ src/main/java/com/mysite/jira/entity/ProjectLikeMembers.java
@@ -0,0 +1,27 @@
+package com.mysite.jira.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ProjectLikeMembers {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "project_like_members_seq")
+	@SequenceGenerator(name = "project_like_members_seq", sequenceName = "project_like_members_seq", allocationSize = 1)
+	private Integer idx;
+	
+	@ManyToOne
+	private Project project;
+	
+	@ManyToOne
+	private Account account;
+}
