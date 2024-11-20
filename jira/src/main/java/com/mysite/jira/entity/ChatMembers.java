aea1a89d40d487aef57919a96ba971bf30a5e35f
ITSC <ITSC@ISTC>
2024-11-20 12:27:05 +0900

    hihi

---
+++ jira/src/main/java/com/mysite/jira/entity/ChatMembers.java
@@ -0,0 +1,34 @@
+package com.mysite.jira.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class ChatMembers {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "chat_members_seq")
+	@SequenceGenerator(name = "chat_members_seq", sequenceName = "chat_members_seq", allocationSize = 1)
+	private Integer idx;
+	
+	@ManyToOne
+	private Account account;
+	
+	@ManyToOne
+	private ChatRoom chatRoom;
+	
+	@Builder
+	public ChatMembers(Account account, ChatRoom chatRoom){
+		this.account = account;
+		this.chatRoom = chatRoom;
+	}
+	
+}
---
+++ jira/src/main/java/com/mysite/jira/entity/DashboardLikeMembers.java
@@ -0,0 +1,26 @@
+package com.mysite.jira.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class DashboardLikeMembers {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "dash_like_members_seq")
+	@SequenceGenerator(name = "dash_like_members_seq", sequenceName = "dash_like_members_seq", allocationSize = 1)
+	private Integer idx;
+	
+	@ManyToOne
+	private Account account;
+	
+	@ManyToOne
+	private Dashboard dashboard; 
+}
