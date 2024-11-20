4da9f07bfa5a5baa1b5b05c9ceee7ad208065330
dahyun0521 <kritac.ndh0521@gmail.com>
2024-11-20 12:11:06 +0900

    Delete jira directory

--- jira/src/main/java/com/mysite/jira/repository/ChatMembersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mysite.jira.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mysite.jira.entity.ChatMembers;
-
-public interface ChatMembersRepository extends JpaRepository<ChatMembers, Integer> {
-
-}
--- jira/src/main/java/com/mysite/jira/repository/ChatRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mysite.jira.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mysite.jira.entity.ChatRoom;
-
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Integer> {
-
-}
