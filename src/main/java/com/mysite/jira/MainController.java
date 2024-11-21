4737636c9928227219f3c3ee7a1eed8a65e46919
ITSC <ITSC@ISTC>
2024-11-21 09:31:05 +0900

    password

--- src/main/java/com/mysite/jira/MainController.java
+++ src/main/java/com/mysite/jira/MainController.java
@@ -2,6 +2,8 @@ package com.mysite.jira;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class MainController {
@@ -9,6 +11,11 @@ public class MainController {
 	@GetMapping("/")
 	public String filter() {
 		return "my_work";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "/account/login";
 	}
 	
 }
