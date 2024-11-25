7a80b2f245075b3f94d9e28454648d1cd40023eb
ITSC <ITSC@ISTC>
2024-11-25 11:34:12 +0900

    합치기

--- src/main/java/com/mysite/jira/repository/IssueRepository.java
+++ src/main/java/com/mysite/jira/repository/IssueRepository.java
@@ -3,9 +3,14 @@ package com.mysite.jira.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.mysite.jira.entity.Issue;
 
 public interface IssueRepository extends JpaRepository<Issue, Integer>{
-	List<Issue> findIssuesByProjectIdx(Integer idx);
+	List<Issue> findIssuesByProjectIdx(Integer idx); 
+	
+	List<Issue> findByJiraIdx(Integer idx);
+	
+	List<String> findNamesByJiraIdx(Integer jiraidx);
 }
---
+++ src/main/java/com/mysite/jira/service/IssueService.java
@@ -0,0 +1,22 @@
+package com.mysite.jira.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.mysite.jira.entity.Issue;
+import com.mysite.jira.repository.IssueRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class IssueService {
+
+	private final IssueRepository issueRepository;
+	
+	public List<Issue> getIssuesByJiraIdx(Integer jiraIdx){
+		return issueRepository.findByJiraIdx(jiraIdx);
+	}
+	
+}
