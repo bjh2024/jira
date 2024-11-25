006d796449ea6f03e0eaf29c1d37626ffe021748
ITSC <ITSC@ISTC>
2024-11-25 11:34:35 +0900

    rgfg

--- src/test/java/com/mysite/jira/JiraApplicationTests.java
+++ src/test/java/com/mysite/jira/JiraApplicationTests.java
@@ -1,30 +1,8 @@
 package com.mysite.jira;
 
-<<<<<<< HEAD
-import java.util.List;
-import java.util.Optional;
-
-=======
->>>>>>> branch 'main' of https://github.com/bjh2024/jira.git
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-<<<<<<< HEAD
-import com.mysite.jira.entity.Account;
-import com.mysite.jira.entity.ChatMembers;
-import com.mysite.jira.entity.ChatMessage;
-import com.mysite.jira.entity.ChatRoom;
-import com.mysite.jira.entity.ChatUnreadList;
-import com.mysite.jira.entity.Issue;
-import com.mysite.jira.repository.AccountRepository;
-import com.mysite.jira.repository.ChatMembersRepository;
-import com.mysite.jira.repository.ChatMessageRepository;
-import com.mysite.jira.repository.ChatRoomRepository;
-import com.mysite.jira.repository.ChatUnreadListRepository;
-import com.mysite.jira.service.BoardMainService;
-
-=======
->>>>>>> branch 'main' of https://github.com/bjh2024/jira.git
 @SpringBootTest
 class JiraApplicationTests {
 	
