04ffc6190908663dcbc9aab8785d33f1f5d49049
ITSC <ITSC@ISTC>
2024-12-18 16:37:26 +0900

    commit

--- src/main/java/com/mysite/jira/controller/AccountAPIController.java
+++ src/main/java/com/mysite/jira/controller/AccountAPIController.java
@@ -1,7 +1,7 @@
 package com.mysite.jira.controller;
 
 import java.util.List;
-
+ 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
